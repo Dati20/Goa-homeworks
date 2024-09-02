0ac2fc76ac5cba2f18d2699d271ac8c9aa753edf
Dati20 <davitimamalashvili.20@gmail.com>
2024-09-02 22:34:04 +0400

    second

---
+++ day17/homework/index.py
@@ -0,0 +1,60 @@
+#ბულიანი არის ორი სახის true and false, ბულიანი არც ტექსტია და არც რიცხვი, არც ბრჭყალები სჭირდება
+
+
+
+a=20
+b=10
+c=10
+d=5
+
+#მეტობა
+
+print(a>b)
+print(a>c)
+print(a>d)
+print(b>c)
+print(b>d)
+
+#ნაკლებობა
+
+print(a<b)
+print(a<c)
+print(a<d)
+print(d<b)
+print(c<b)
+
+
+#ნაკლებია ან ტოლია
+
+print(a<=b)
+print(b<=c)
+print(c<=d)
+print(d<=a)
+print(a<=c)
+
+# მეტია ან ტოლია
+
+print(b>=c)
+print(a>=d)
+print(c>=d)
+print(b>=d)
+print(a>=b)
+
+
+
+#არ უდრის
+
+print(a!=b)
+print(b!=d)
+print(b!=c)
+print(a!=d)
+print(c!=d)
+
+# უდრის
+print(a==b)
+print(a==c)
+print(a==d)
+print(c==d)
+print(b==d)
+
+
